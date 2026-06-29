38ce5736c4eba3887c0acfdeda817b05c114d106
Hosein <hshafie512@gmail.com>
2026-06-29 13:12:53 +0330

    feat: add commit functionality with date and time

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const tabs = document.querySelectorAll('button[id^="tab-"]')
 const contents = document.querySelectorAll('div[id^="content-"]')
 
+const input = document.getElementById('thought-input')
+const commitBtn = document.getElementById('commit-btn')
+const commitList = document.getElementById('content-commit')
+
 contents.forEach(function(content) {
   content.classList.add('hidden')
 })
@@ -26,3 +30,27 @@ document.getElementById(id).classList.remove('hidden')
 
   })
 })
+
+commitBtn.addEventListener('click', function() {
+  const text = input.value
+  
+  if(text === '') {
+    alert('چیزی ننوشتی!')
+    return
+  }
+  
+const now = new Date()
+const date = now.toLocaleDateString('fa-IR')
+const time = now.toLocaleTimeString('fa-IR')
+
+const div = document.createElement('div')
+div.innerHTML = `
+ <div class="flex flex-col gap-1 py-4 border-b border-zinc-100">
+    <p class="text-sm text-zinc-800">${text}</p>
+    <span class="mono text-xs text-zinc-400">${date} — ${time}</span>
+  </div>
+`
+commitList.appendChild(div)
+input.value = ''
+})
+
